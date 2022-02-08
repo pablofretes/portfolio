6d018c5a819de91459a80b06634f837064a4f9cc
Pablo Fretes <pablofretes.balzarelli@gmail.com>
2022-02-08 04:40:25 -0300

    Adds language state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
 import Home from './components/Home';
 import NavBar from './components/Navbar';
+import HomeEnglish from './components/English/HomeEnglish';
+import NavbarEnglish from './components/English/NavbarEnglish';
 
 const App = () => {
+  const [language, setLanguage] = useState('Spanish');
   return (
     <div>
-      <NavBar />
-      <Home />
+      {language === 'Spanish' ? (
+        <div>
+          <NavBar setLanguage={setLanguage}/>
+          <Home />
+        </div>
+      ) : (
+        <div>
+          <NavbarEnglish setLanguage={setLanguage}/>
+          <HomeEnglish />
+        </div>
+      )}
     </div>
   );
 }

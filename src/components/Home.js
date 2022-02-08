accc4cced7fc9064abe8044492fea1c7532c6a2e
Pablo Fretes <pablofretes.balzarelli@gmail.com>
2022-02-08 04:40:08 -0300

    Adds about
    Home section was looking a bit empty and a little about me couldn't hurt anyone

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,15 @@ const Home = () => {
                     Hola, Soy Pablo ;)
                 </h1>
                 <h2 className='text-titulo'>
-                    Soy Diseñador / FullStack Web Developer. 
+                    Soy Diseñador / Frontend Web Developer. 
                 </h2>
+                <p className='about-me'>
+                    Me entere de lo que era programación muy por arriba cuando empezó la pandemia.
+                    Mi mamá se había puesto un pequeño negocio de cosmética desde casa y pensé "¿no estaría bueno hacerle un sitio web?,
+                    no puede ser muy dificil". Me equivoqué, era muy dificil, cada vez que me fijaba algo me daba mas cuenta de lo
+                    complicado que era, yo todo se volvía cada vez mas difícil. Al final mi mamá se aburrió de su pequeño negocio, pero yo no me pude despegar
+                    mas de la programación. Y ahora me gustaria llevar lo que aprendí al mundo profesional. Contactame! ;).
+                </p>
                 <div className='home-links'>
 						<a href='/PabloFretes-CV.pdf' download='PabloFretes-CV.pdf' target='_blank' className='demo-button'>
 							Descargar CV <FiDownload className='social-icon' />

32bc161ef16cb1c596e9cc4fb508fd6125a9f852
Pablo Fretes <pablofretes.balzarelli@gmail.com>
2022-02-13 02:10:10 -0300

    Separates project info
    because it looks better

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -26,24 +26,28 @@ const Slider = ({ data }) => {
     }
 
     return (
-        <div className='slider-container'>
-            <div key={data[slideIndex].id} className={slideIndex === data[slideIndex].id - 1 ? 'project-container active-slide' : ''}>
-                <div className='img-container'>
-                    <img src={data[slideIndex].img} alt='' className='project-image' />
+        <div className='slider-root'>
+            <div className='slider-container'>
+                <div key={data[slideIndex].id} className={slideIndex === data[slideIndex].id - 1 ? 'project-container active-slide' : ''}>
+                    <div className='img-container'>
+                        <img src={data[slideIndex].img} alt='' className='project-image' />
+                    </div>
+                    <div className='text-container'>
+                        <h3 className='project-title text-titulo'>{data[slideIndex].name} </h3>
+                        <p className='project-description'>{data[slideIndex].description}</p>
+                    </div>
+                    <div className='project-links'>
+                        <a href={data[slideIndex].demo} target='_blank' rel='noreferrer'>
+                            <button className='demo-button'>Demo</button>
+                        </a>
+                        <a href={data[slideIndex].github} target='_blank' rel='noreferrer'>
+                            <button className='code-button'>Código</button>
+                        </a>
+                    </div>
                 </div>
-                <div className='text-container'>
-                    <h3 className='project-title text-titulo'>{data[slideIndex].name}</h3>
-                    <p className='project-info'>{data[slideIndex].info}</p>
-                    <p className='project-description'>{data[slideIndex].description}</p>
-                </div>
-                <div className='project-links'>
-                    <a href={data[slideIndex].demo} target='_blank' rel='noreferrer'>
-                        <button className='demo-button'>Demo</button>
-                    </a>
-                    <a href={data[slideIndex].github} target='_blank' rel='noreferrer'>
-                        <button className='code-button'>Código</button>
-                    </a>
-                </div>
+            </div>
+            <div className={slideIndex === data[slideIndex].id - 1 ? 'project-info-container active-slide' : ''}>
+                <p className='project-info'>{data[slideIndex].info}</p>
             </div>
             <ButtonSlider slide={previousSlide} direction='previous'/>
             <ButtonSlider slide={nextSlide} direction='next'/>

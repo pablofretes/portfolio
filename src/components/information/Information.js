de8f6e257395326ce42ee452ebbbf531d850d050
Pablo Fretes <pablofretes.balzarelli@gmail.com>
2022-02-24 00:24:13 -0300

    shadows

--- src/components/information/Information.js
+++ src/components/information/Information.js
@@ -2,67 +2,63 @@ import React from "react";
 import './information.css';
 
 const Information = ({ language }) => {
-    return(
-        <>
-            {language === 'Spanish' ? (
-                <div className='container-info' data-cy='information-section'>
-                    <h2 className='text-titulo'>Mas cosas que saber sobre mi</h2>
-                    <ul>
-                        <li className='listed-items'>
-                            <a
-                                href='https://www.efset.org/cert/wqpnK1'
-                                target='_blank'
-                                rel="noreferrer"
-                                style={{ textDecoration: 'none' } }
-                                data-cy='link-ingles'
-                            >
-                            Ingles bilingüe.
-                            </a>
-                        </li>
-                        <li className='listed-items'>
-                            <a
-                                href='https://github.com/pablofretes/fullstackopen/'
-                                target='_blank'
-                                rel="noreferrer"
-                                style={{ textDecoration: 'none' } }
-                                data-cy='link-fullstackopen'
-                            >
-                            Curso completo de FullStack Open 2021 
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            ) : (
-                <div className='container-info' data-cy='information-section'>
-                    <h2 className='text-titulo'>Some more info about me</h2>
-                    <ul>
-                        <li className='listed-items'>
-                            <a
-                                href='https://www.efset.org/cert/wqpnK1'
-                                target='_blank'
-                                rel="noreferrer"
-                                style={{ textDecoration: 'none' } }
-                                data-cy='link-ingles'
-                            >
-                            Bilingual English
-                            </a>
-                        </li>
-                        <li className='listed-items'>
-                            <a
-                                href='https://github.com/pablofretes/fullstackopen/'
-                                target='_blank'
-                                rel="noreferrer"
-                                style={{ textDecoration: 'none' } }
-                                data-cy='link-fullstackopen'
-                            >
-                            Completed FullStack 2021 Course
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            )}
-        </>
-    );
+  return(
+    <>
+      {language === 'Spanish' ? (
+        <div className='container-info' data-cy='information-section'>
+          <h2 className='text-titulo'>Mas cosas que saber sobre mi</h2>
+            <p className='listed-items'>
+              <a
+                href='https://www.efset.org/cert/wqpnK1'
+                target='_blank'
+                rel="noreferrer"
+                style={{ textDecoration: 'none' } }
+                data-cy='link-ingles'
+              >
+              Ingles bilingüe.
+              </a>
+            </p>
+            <p className='listed-items'>
+              <a
+                href='https://github.com/pablofretes/fullstackopen/'
+                target='_blank'
+                rel="noreferrer"
+                style={{ textDecoration: 'none' } }
+                data-cy='link-fullstackopen'
+              >
+              Curso completo de FullStack Open 2021 
+              </a>
+            </p>
+        </div>
+      ) : (
+        <div className='container-info' data-cy='information-section'>
+          <h2 className='text-titulo'>Some more info about me</h2>
+            <p className='listed-items'>
+              <a
+                href='https://www.efset.org/cert/wqpnK1'
+                target='_blank'
+                rel="noreferrer"
+                style={{ textDecoration: 'none' } }
+                data-cy='link-ingles'
+              >
+              Bilingual English
+              </a>
+            </p>
+            <p className='listed-items'>
+              <a
+                href='https://github.com/pablofretes/fullstackopen/'
+                target='_blank'
+                rel="noreferrer"
+                style={{ textDecoration: 'none' } }
+                data-cy='link-fullstackopen'
+              >
+              Completed FullStack 2021 Course
+              </a>
+            </p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Information;
